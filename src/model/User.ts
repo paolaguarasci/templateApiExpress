3110aafce6f154f2dbeb38d3356f731b06ba8519
Paola Guarasci <paolaguarasci@gmail.com>
2022-10-28 13:08:27 +0200

    user model and test

--- src/model/User.ts
+++ src/model/User.ts
@@ -1,9 +1,24 @@
-import { RoleType } from "./RoleType";
+import { RoleType } from './RoleType';
 
-export interface User {
+export class User {
   readonly username: string;
   hash: string;
   token: string;
   role: RoleType;
-}
 
+  constructor(username: string, hash: string, token: string, role: RoleType) {
+    this.username = this.sanitizeAndValidateUsername(username);
+    this.hash = hash;
+    this.token = token;
+    this.role = role;
+  }
+
+  private sanitizeAndValidateUsername(username: string): string {
+    username = username.trim();
+    let usernameRegExp = /^[A-Za-z0-9_]{8,16}$/;
+    if (username.length < 8 || username.length > 16)
+      throw new Error('Username invalid');
+    if (!usernameRegExp.test(username)) throw new Error('Username invalid');
+    return username;
+  }
+}
--- src/test/model/user.spec.ts
+++ src/test/model/user.spec.ts
@@ -1,8 +1,9 @@
 import 'mocha';
 
+import chai, { expect } from 'chai';
+
 import { RoleType } from '../../model/RoleType.js';
 import { User } from '../../model/User.js';
-import chai from 'chai';
 import chaiHttp from 'chai-http';
 
 chai.use(chaiHttp);
@@ -10,14 +11,7 @@ chai.should();
 
 describe('MODEL USER', () => {
   before(async function () {
-    let user: User = {
-      username: 'asas',
-      token: '',
-      role: RoleType.Admin,
-      hash: ''
-    };
-
-    this.user = user;
+    this.user = new User('paola12345', '', '', RoleType.Admin);
   });
 
   it('username props exists as a string', async function () {
@@ -34,5 +28,19 @@ describe('MODEL USER', () => {
 
   it('role props exists as a RoleType', async function () {
     this.user.should.haveOwnProperty('role').to.be.a(typeof RoleType.Admin);
+  });
+
+  it('username should be between 8 and 16 alphanumeric characters long', async function () {
+    expect(() => {
+      new User('paola', '', '', RoleType.Admin);
+    }).to.throw('Username invalid');
+
+    expect(() => {
+      new User('paola#$%^', '', '', RoleType.Admin);
+    }).to.throw('Username invalid');
+
+    expect(() => {
+      new User('paola123', '', '', RoleType.Admin);
+    }).to.not.throw();
   });
 });
